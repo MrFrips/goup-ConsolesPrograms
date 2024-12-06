be6b3fee8e2a333ddcf99823e48583969026bb7c
MrFrips <37955209+MrFrips@users.noreply.github.com>
2024-12-06 23:04:07 +0300

    FirstPost

---
+++ all-goup-console-program/1LAB-shstart.cs
@@ -0,0 +1,8 @@
+﻿/*
+static void Main(string[] args)
+{
+    Console.WriteLine("Default 1234");
+    Console.Write("Choose ur character pls: ");
+    double user_choose = Convert.ToInt32(Console.ReadLine());
+}
+*/
---
+++ all-goup-console-program/LABFirst.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace all_goup_console_program
+{
+    internal class LABFirst
+    {
+        static void Main(string[] args)
+        {
+            int A, P;
+
+            Console.Write("Введите трехзначное число A: ");
+            A = int.Parse(s: Console.ReadLine());
+
+            // Проверяем, является ли число трехзначным
+            if (A < 100 || A > 999)
+            {
+                Console.WriteLine("Err: not a 3.");
+                return;
+            }
+
+            // Проверяем, входит ли цифра 5 в число A
+            if (A / 100 == 5 || (A / 10) % 10 == 5 || A % 10 == 5)
+            {
+                P = 1;
+            }
+            else
+            {
+                P = 0;
+            }
+
+            Console.WriteLine($"Res P: {P}");
+            /*
+            Console.WriteLine("Default > 5");
+            Console.Write("Write pls x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Default > 5");
+            Console.Write("Write pls y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            // Вычисление значения функции Z
+            double z = (Math.Pow(Math.Sin(x), 5) * Math.Pow(Math.Cos(y), 5)) / 25 +
+            (Math.Pow(Math.Sin(x), 3) - Math.Pow(Math.Cos(y), 3)) / 125;
+
+            Console.WriteLine("result Z: " + z);
+            Console.ReadLine();
+            AMain();
+            */
+        }
+    }
+}
